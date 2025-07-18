b1cf38a1f5ab09b3ad9b20801395a3f0a526124a
Danyss09 <112229585+Danyss09@users.noreply.github.com>
2025-07-18 16:55:54 -0500

    Update clasificador_riesgo.py

--- scripts/clasificador_riesgo.py
+++ scripts/clasificador_riesgo.py
@@ -2,12 +2,10 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-def entrenar_modelo(csv_path, salida_modelo):
+def entrenar_modelo(csv_path, modelo_path):
     df = pd.read_csv(csv_path)
-    X = df.drop(columns=['Clase', 'Archivo'])
+    X = df.drop(columns=['Archivo', 'Clase'])
     y = df['Clase']
     modelo = RandomForestClassifier(n_estimators=100, random_state=42)
     modelo.fit(X, y)
-    joblib.dump(modelo, salida_modelo)
-    print(f"Modelo guardado en {salida_modelo}")
-
+    joblib.dump(modelo, modelo_path)
